9caa0778b6972f2c71c61e9212cc1eba98931f42
saseungmin <dbd02169@naver.com>
2021-02-01 18:09:16 +0900

    [Feature] Initial project settings for Todo Application - setting for eslint - npm install for testing, babel, webpack, react, recoil.. etc - starting with webpack-dev-server

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+import '../assets/global.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+const DEVELOPMENT_ENV = 'development';
+const PRODUCTION_ENV = 'production';
+
+const mode = process.env.NODE_ENV || DEVELOPMENT_ENV;
+
+const appBuild = path.resolve(__dirname, 'build');
+const appSrc = path.resolve(__dirname, 'src');
+const appIndex = path.resolve(__dirname, 'src', 'index.jsx');
+const appHtml = path.resolve(__dirname, 'public', 'index.html');
+
+module.exports = {
+  mode,
+  devtool: 'cheap-eval-source-map',
+  entry: appIndex,
+  output: {
+    path: appBuild,
+    filename: mode === PRODUCTION_ENV
+      ? 'static/js/[name].[contenthash:8].js'
+      : mode === DEVELOPMENT_ENV && 'static/js/bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+        include: appSrc,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    overlay: true,
+    historyApiFallback: true,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: appHtml,
+      templateParameters: {
+        env: mode === DEVELOPMENT_ENV ? '(개발용)' : '',
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new Dotenv(),
+  ],
+};

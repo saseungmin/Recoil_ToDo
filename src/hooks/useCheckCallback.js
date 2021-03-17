c7b13eebeb65133ff176ddda43044cd353480a8d
saseungmin <dbd02169@naver.com>
2021-03-17 19:47:47 +0900

    [Fix] Token check loading at first rendering
    - 첫 랜더링 시 유저의 토큰을 검사하는 로직 탈때, 로딩 표시
    - 커스텀 useCheckCallback 생성

---
+++ src/hooks/useCheckCallback.js
@@ -0,0 +1,17 @@
+import { useRecoilCallback } from 'recoil';
+
+import userAtom, { userWithCheck } from '../recoil/user';
+import isLoadingAtom from '../recoil/common/atom';
+
+import { userCheckErrorHandling } from '../utils/utils';
+
+const useCheckCallback = () => useRecoilCallback(({ snapshot, set, reset }) => async () => {
+  set(isLoadingAtom, true);
+
+  const { data } = await userCheckErrorHandling(snapshot.getPromise(userWithCheck()));
+
+  set(userAtom, { user: data });
+  reset(isLoadingAtom);
+}, []);
+
+export default useCheckCallback;

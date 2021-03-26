dc7490916f64edc88019ea843c4b2c9844c8a300
saseungmin <dbd02169@naver.com>
2021-03-26 18:26:56 +0900

    [Feature] html meta and Webpack optimization - Add html meta tags and Add favicon - Install dependency with copy-webpack-plugin, mini-css-extract-plugin, optimize-css-assets-webpack-plugin, terser-webpack-plugin - Add logo images

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
-const path = require('path');
+const webpack = require('webpack');
 
+const path = require('path');
+const childProcess = require('child_process');
+
+const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-const DEVELOPMENT_ENV = 'development';
-const PRODUCTION_ENV = 'production';
+const DEVELOPMENT = 'development';
+const PRODUCTION = 'production';
 
-const mode = process.env.NODE_ENV || DEVELOPMENT_ENV;
+const mode = process.env.NODE_ENV || DEVELOPMENT;
 
 const appBuild = path.resolve(__dirname, 'build');
 const appSrc = path.resolve(__dirname, 'src');
 const appIndex = path.resolve(__dirname, 'src', 'index.jsx');
 const appHtml = path.resolve(__dirname, 'public', 'index.html');
+const favicon = path.resolve(__dirname, 'public', 'favicon.ico');
 
 module.exports = {
   mode,
@@ -19,18 +27,19 @@ module.exports = {
   entry: appIndex,
   output: {
     path: appBuild,
-    filename: mode === PRODUCTION_ENV
+    filename: mode === PRODUCTION
       ? 'static/js/[name].[contenthash:8].js'
-      : mode === DEVELOPMENT_ENV && 'static/js/bundle.js',
-    publicPath: mode === PRODUCTION_ENV ? '/Recoil_ToDo/' : mode === DEVELOPMENT_ENV && '/',
+      : mode === DEVELOPMENT && 'static/js/bundle.js',
+    publicPath: mode === PRODUCTION ? '/Recoil_ToDo/' : mode === DEVELOPMENT && '/',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [
-          'style-loader',
-          'css-loader',
+          mode === PRODUCTION
+            ? MiniCssExtractPlugin.loader
+            : 'style-loader', 'css-loader',
         ],
       },
       {
@@ -48,6 +57,9 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  externals: {
+    axios: 'axios',
+  },
   devServer: {
     hot: true,
     overlay: true,
@@ -59,12 +71,53 @@ module.exports = {
     },
   },
   plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+        Build Date: ${new Date().toLocaleString()}
+        Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
+        Author: ${childProcess.execSync('git config user.name')}
+        Author-Email: ${childProcess.execSync('git config user.email')}
+      `,
+    }),
     new HtmlWebpackPlugin({
       template: appHtml,
       templateParameters: {
-        env: mode === DEVELOPMENT_ENV ? '(개발용)' : '',
+        env: mode === DEVELOPMENT ? '(개발용)' : '',
       },
+      minify: mode === PRODUCTION ? {
+        collapseWhitespace: true,
+        removeComments: true,
+      } : false,
+      favicon,
     }),
     new CleanWebpackPlugin(),
+    ...(mode === PRODUCTION
+      ? [new MiniCssExtractPlugin({ filename: 'static/css/[name].[contenthash:8].css' })]
+      : []),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './node_modules/axios/dist/axios.min.js',
+          to: './static/js/axios.min.js',
+        },
+        {
+          from: './public/assets',
+          to: './assets',
+        },
+      ],
+
+    }),
   ],
+  optimization: {
+    minimizer: mode === PRODUCTION ? [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+    ] : [],
+  },
 };
